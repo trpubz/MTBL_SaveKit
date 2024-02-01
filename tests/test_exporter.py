dce3727b5be789cf2786dbb0070a8bf8415356f1
Taylor Pubins <tpubz@icloud.com>
2024-02-01 15:09:05 -0700

    Complete testing

--- tests/test_exporter.py
+++ tests/test_exporter.py
@@ -6,20 +6,31 @@ from mtbl_savekit import exporter as sk
 
 class TestExporter:
     df = None
+    full_path = ""
 
     @pytest.fixture(autouse=True)
-    def setup(self):
+    def setup_and_teardown(self):
         self.df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+        yield
+        os.remove(self.full_path)
 
     def test_export_dataframe_to_csv(self):
-        sk.export_dataframe(self.df, "test", ".json", directory="./temp")
-        full_path = "./temp/test.json"
-        assert os.path.exists(full_path)
-        os.remove(full_path)
+        sk.export_dataframe(self.df, "test", ".csv", "./temp")
+        self.full_path = "./temp/test.csv"
+        assert os.path.exists(self.full_path)
 
     def test_export_dataframe_to_json(self):
-        sk.export_dataframe(self.df, "test", ".csv", directory="./temp")
-        full_path = "./temp/test.csv"
-        assert os.path.exists(full_path)
-        os.remove(full_path)
+        sk.export_dataframe(self.df, "test", ".json", "./temp")
+        self.full_path = "./temp/test.json"
+        assert os.path.exists(self.full_path)
+
+    def test_export_dataframe_to_json_without_schema(self):
+        sk.export_dataframe(self.df, "test", ".json", "./temp", schema=False)
+        self.full_path = "./temp/test.json"
+        assert os.path.exists(self.full_path)
+
+    def test_export_dataframe_without_proper_file_type(self):
+        sk.export_dataframe(self.df, "test", "json", "./temp")
+        self.full_path = "./temp/test.json"
+        assert os.path.exists(self.full_path)
 
